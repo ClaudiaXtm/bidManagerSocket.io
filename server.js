49284c52c0f29c268504565a43bde071b39a84c3
Claudia Piscociu <claudia2xtm@gmail.com>
2018-07-05 00:24:09 +0300

    04 JULY

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongo = require('mongodb').MongoClient;
 const client = require('socket.io').listen(4000).sockets;
 
 // Connect to mongo
-mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
+mongo.connect('mongodb://127.0.0.1/auction_doc', function(err, db){
     if(err){
         throw err;
     }
@@ -11,41 +11,46 @@ mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
 
     // Connect to Socket.io
     client.on('connection', function(socket){
-        let chat = db.collection('chats');
+        let bid = db.collection('bids');
 
         // Create function to send status
         sendStatus = function(s){
             socket.emit('status', s);
         }
 
-        // Get chats from mongo collection
-        chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
+        // Get bids from mongo collection
+        bid.find().limit(100).sort({_id:1}).toArray(function(err, res){
             if(err){
                 throw err;
             }
 
-            // Emit the messages
+            // Emit the new_bids
             socket.emit('output', res);
         });
 
         // Handle input events
         socket.on('input', function(data){
             let auction_name = data.auction_name;
-            let message = data.message;
+            let new_bid = data.new_bid;
           //  let auction_time = data.auction_time.getHours()+ ":" + data.auction_time.getMinutes() + ":" + data.auction_time.getSeconds();
 
-            // Check for name and message
-            if(auction_name == '' || message == ''){
+            // Check for name and new_bid
+            if(auction_name == '' || new_bid == '' ){
                 // Send error status
-                sendStatus('Please enter a name and message');
-            } else {
-                // Insert message
-                chat.insert({auction_name: auction_name, message: message}, function(){
+                sendStatus('Please enter a name and new bid');
+            }
+            else if(isNaN(new_bid)){
+              sendStatus('The bid must be numeric!!!');
+            }
+
+            else {
+                // Insert new_bid
+                bid.insert({auction_name: auction_name, new_bid: new_bid}, function(){
                     client.emit('output', [data]);
 
                     // Send status object
                     sendStatus({
-                        message: 'Bid placed',
+                        new_bid: 'Bid placed',
                         clear: true
                     });
                 });
@@ -54,8 +59,8 @@ mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
 
         // Handle clear
         socket.on('clear', function(data){
-            // Remove all chats from collection
-            chat.remove({}, function(){
+            // Remove all bids from collection
+            bid.remove({}, function(){
                 // Emit cleared
                 socket.emit('cleared');
             });
